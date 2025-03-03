869dd14e58fe321c86abd7515a4315d5110d4c6e
ESTUDIANTE <julianandresguerreromejia144@gmail.com>
2025-03-02 19:46:11 -0500

    cambie algo

--- pagina-web-profesional/assets/js/script.js
+++ pagina-web-profesional/assets/js/script.js
@@ -1,5 +1,9 @@
 // Este archivo contiene el código JavaScript para la página web. Se utiliza para agregar interactividad y funcionalidades dinámicas.
 
 const validarUsuario = (valor,usuario)=>{
-    
+    valor = prompt( "cual es el valor ?")
+    usuario = []
+    for(i = 0; i<5; i++){
+        usuario = [prompt("dime tu nombre")]
+    }
 }
