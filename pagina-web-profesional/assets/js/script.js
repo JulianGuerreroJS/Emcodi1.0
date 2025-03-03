4a593a68215faf246214d896739e855ba11d100a
ESTUDIANTE <julianandresguerreromejia144@gmail.com>
2025-03-02 19:39:31 -0500

    hice algunas cosas

--- pagina-web-profesional/assets/js/script.js
+++ pagina-web-profesional/assets/js/script.js
@@ -1,1 +1,5 @@
 // Este archivo contiene el código JavaScript para la página web. Se utiliza para agregar interactividad y funcionalidades dinámicas.
+
+const validarUsuario = (valor,usuario)=>{
+    
+}
